839ee47c7006fbc1d7bca8c7a7ff526d4f6346f2
Hlioskjalf <a6m3.zero.sen@gmail.com>
2025-04-05 12:19:26 +0300

    05.04.25

---
+++ Practic/Zadanie7/Zadanie7.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Zadanie7
+{
+    partial class Zadanie7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.eatenChickenLabel = new System.Windows.Forms.Label();
+            this.chickensLeftLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 9;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.4F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.8F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.8F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.2F));
+            this.tableLayoutPanel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(500, 400);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // eatenChickenLabel
+            // 
+            this.eatenChickenLabel.AutoSize = true;
+            this.eatenChickenLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eatenChickenLabel.Location = new System.Drawing.Point(7, 422);
+            this.eatenChickenLabel.Name = "eatenChickenLabel";
+            this.eatenChickenLabel.Size = new System.Drawing.Size(178, 30);
+            this.eatenChickenLabel.TabIndex = 1;
+            this.eatenChickenLabel.Text = "Eaten chickens: ?";
+            // 
+            // chickensLeftLabel
+            // 
+            this.chickensLeftLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.chickensLeftLabel.AutoSize = true;
+            this.chickensLeftLabel.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chickensLeftLabel.Location = new System.Drawing.Point(385, 422);
+            this.chickensLeftLabel.Name = "chickensLeftLabel";
+            this.chickensLeftLabel.Size = new System.Drawing.Size(127, 30);
+            this.chickensLeftLabel.TabIndex = 2;
+            this.chickensLeftLabel.Text = "Chickens: ? ";
+            // 
+            // Zadanie7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(524, 461);
+            this.Controls.Add(this.chickensLeftLabel);
+            this.Controls.Add(this.eatenChickenLabel);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Zadanie7";
+            this.Text = "Foxes and chickens";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label eatenChickenLabel;
+        private System.Windows.Forms.Label chickensLeftLabel;
+    }
+}
+
---
+++ Practic/Zadanie7/Zadanie7.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Zadanie7
+{
+    public partial class Zadanie7 : Form
+    {
+        private const int BoardSize = 7;
+
+        private GameBoardFacade _gameBoardFacade;
+        private Point _selectedPiece;
+        private PictureBox _selectedPictureBox;
+
+        public Zadanie7()
+        {
+            InitializeComponent();
+            CreateBoard();
+            InitializeGame();
+
+            foreach (Control control in tableLayoutPanel1.Controls)
+                if (control is PictureBox cellPictureBox) 
+                    cellPictureBox.Click += Cell_Click;
+
+            HelpButtonClicked += ShapedForm1_HelpButtonClicked;
+
+            KeyPreview = true;
+            KeyDown += Task7_KeyDown;
+        }
+
+        private void Task7_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space && _selectedPiece != Point.Empty)
+            {
+                ClearSelection();
+                UpdateUi();
+            }
+        }
+
+        private void ClearSelection()
+        {
+            ClearHighlighting();
+            _selectedPiece = Point.Empty;
+            _selectedPictureBox = null; 
+        }
+
+        private void CreateBoard()
+        {
+            tableLayoutPanel1.Controls.Clear();
+            tableLayoutPanel1.ColumnStyles.Clear();
+            tableLayoutPanel1.RowStyles.Clear();
+
+            tableLayoutPanel1.ColumnCount = BoardSize;
+            tableLayoutPanel1.RowCount = BoardSize;
+
+            for (int i = 0; i < BoardSize; i++)
+            {
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / BoardSize));
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / BoardSize));
+            }
+
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    PictureBox cellPictureBox = new PictureBox
+                    {
+                        Dock = DockStyle.Fill,
+                        SizeMode = PictureBoxSizeMode.Zoom,
+                        BorderStyle = BorderStyle.FixedSingle,
+                        Margin = new Padding(0),
+                        Padding = new Padding(0),
+                        BackColor = Color.White
+                    };
+
+                    // Making corner gray cells inaccessible
+                    if ((row < 2 && (col < 2 || col > 4)) || (row > 4 && (col < 2 || col > 4)))
+                    {
+                        cellPictureBox.BackColor = Color.Gray;
+                        cellPictureBox.Enabled = false;
+                    }
+
+                    cellPictureBox.Click += Cell_Click;
+                    tableLayoutPanel1.Controls.Add(cellPictureBox, col, row);
+                }
+            }
+        }
+
+        private void InitializeGame()
+        {
+            _gameBoardFacade = new GameBoardFacade(BoardSize);
+            _selectedPiece = Point.Empty;
+            _selectedPictureBox = null;
+            UpdateUi();
+        }
+
+        private void ShapedForm1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            if (MessageBox.Show("Goal: chickens must occupy the top 3x3 square, and the foxes must eat as many chickens as the chickens need to win." +
+                "\nChickens moves first. Foxes move after chickens and must eat them if there'r possibility. " +
+                "\n\nDeselect - spacebar", "Foxes and chickens", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                e.Cancel = true;
+        }
+
+        private void Cell_Click(object sender, EventArgs e)
+        {
+            if (!(sender is PictureBox clickedCell)) return;
+
+            Point cellLocation = GetCellLocation(clickedCell);
+
+            if (_gameBoardFacade.CurrentPlayer == PlayerType.Chicken)
+                HandleChickenMove(cellLocation, clickedCell);
+            else
+                HandleFoxMove(cellLocation, clickedCell);
+        }
+
+        private Point GetCellLocation(PictureBox cell)
+        {
+            TableLayoutPanelCellPosition position = tableLayoutPanel1.GetPositionFromControl(cell);
+            return new Point(position.Column, position.Row);
+        }
+
+        private void HandleChickenMove(Point cellLocation, PictureBox clickedCell)
+        {
+            if (_selectedPiece == Point.Empty)
+            {
+                if (_gameBoardFacade.GetCellType(cellLocation) == CellType.Chicken)
+                {
+                    if (_gameBoardFacade.CanChickenMove(cellLocation))
+                    {
+                        _selectedPiece = cellLocation;
+                        _selectedPictureBox = clickedCell;
+                        HighlightSelectedPiece();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No moves available.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        _selectedPiece = Point.Empty;
+                        _selectedPictureBox = null; 
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Choose a chicken.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                if (_gameBoardFacade.IsValidChickenMove(_selectedPiece, cellLocation))
+                {
+                    _gameBoardFacade.MoveChicken(_selectedPiece, cellLocation);
+                    ClearHighlighting();
+                    _selectedPiece = Point.Empty;
+                    _selectedPictureBox = null; 
+                    MakeFoxMove();
+                }
+                else
+                {
+                    MessageBox.Show("Inadmissible movement.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            UpdateUi();
+            CheckForGameOver();
+        }
+
+        private void HandleFoxMove(Point cellLocation, PictureBox clickedCell) 
+        {
+            if (_selectedPiece == Point.Empty)
+            {
+                if (_gameBoardFacade.GetCellType(cellLocation) == CellType.Fox)
+                {
+                    _selectedPiece = cellLocation;
+                    _selectedPictureBox = clickedCell; 
+                    HighlightSelectedPiece();
+                }
+                else
+                    MessageBox.Show("Select a fox.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (_gameBoardFacade.IsValidFoxMove(_selectedPiece, cellLocation))
+                {
+                    _gameBoardFacade.MoveFox(_selectedPiece, cellLocation);
+                    ClearHighlighting();
+                    _selectedPiece = Point.Empty;
+                    _selectedPictureBox = null;
+
+                    _gameBoardFacade.SwitchPlayer();
+                }
+                else
+                {
+                    MessageBox.Show("Inadmissible movement.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            UpdateUi();
+            CheckForGameOver();
+        }
+
+        private void MakeFoxMove()
+        {
+            bool foxMoved = true;
+            while (foxMoved && _gameBoardFacade.CurrentPlayer == PlayerType.Fox)
+            {
+                foxMoved = _gameBoardFacade.MakeFoxMove();
+                UpdateUi();
+                CheckForGameOver();
+
+                if (!foxMoved && _gameBoardFacade.CurrentPlayer == PlayerType.Fox)
+                {
+                    foxMoved = _gameBoardFacade.MakeRandomFoxMove();
+                }
+
+                if (!foxMoved && _gameBoardFacade.CurrentPlayer == PlayerType.Fox)
+                {
+                    MessageBox.Show("Winner winner chicken dinner!.", "Game over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ResetGame();
+                    return;
+                }
+
+            }
+        }
+
+        private void UpdateUi()
+        {
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    if (!(tableLayoutPanel1.GetControlFromPosition(col, row) is PictureBox cell)) continue;
+
+                    CellType cellType = _gameBoardFacade.GetCellType(new Point(col, row));
+
+                    if (cellType == CellType.Chicken)
+                        cell.Image = Image.FromFile("chicken.png");
+                    else if (cellType == CellType.Fox)
+                        cell.Image = Image.FromFile("fox.png");
+                    else
+                        cell.Image = null;
+                }
+            }
+
+            eatenChickenLabel.Text = "Eaten chickens: " + _gameBoardFacade.EatenChickensCount;
+            chickensLeftLabel.Text = "Chickens: " + _gameBoardFacade.ChickenCount;
+        }
+
+        private void CheckForGameOver()
+        {
+            if (_gameBoardFacade.ChickenCount <= 8)
+            {
+                MessageBox.Show("Foxes won! Chickens don't have space.", "Game over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ResetGame();
+                return;
+            }
+            if (_gameBoardFacade.CheckChickenWin())
+            {
+                MessageBox.Show("Chickens won! They filled top square.", "Game over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ResetGame();
+                return;
+            }
+            if (_gameBoardFacade.CurrentPlayer == PlayerType.Chicken && !_gameBoardFacade.CanChickensMove())
+            {
+                MessageBox.Show("Foxes won! Chickens can't move it move it.", "Game over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ResetGame();
+                return;
+            }
+            if (_gameBoardFacade.CurrentPlayer == PlayerType.Fox && !_gameBoardFacade.CanFoxesMove())
+            {
+                MessageBox.Show("Chickens won! Foxes can't move it move it.", "Game over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ResetGame();
+                return;
+            }
+        }
+
+        private void ResetGame()
+        {
+            InitializeGame();
+            UpdateUi();
+        }
+
+        private void HighlightSelectedPiece()
+        {
+            if (_selectedPictureBox != null) 
+                _selectedPictureBox.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void ClearHighlighting()
+        {
+            if (_selectedPictureBox != null)
+                _selectedPictureBox.BorderStyle = BorderStyle.FixedSingle;
+        }
+    }
+
+    public enum CellType
+    {
+        Empty,
+        Chicken,
+        Fox
+    }
+
+    public enum PlayerType
+    {
+        Chicken,
+        Fox
+    }
+}
